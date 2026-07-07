e6a79d406e6d4694533c85bf31df20b3a5fae0bd
Jeremy Rogers <info@jero.co.nz>
2026-07-07 15:08:41 +1200

    Add warning message

--- subscribe/index.php
+++ subscribe/index.php
@@ -25,6 +25,7 @@ require("head.inc");
 <div id="sub_wrapper">
 <div id="subscribediv">
 <p class="heading_large">Subscribe Online</p>
+    <p><strong>Please email us on  <a href="mailto:info@nzoutdoor.co.nz">info@nzoutdoor.co.nz</a> or phone <a href="tel:+64274871447">+64 274 871447</a> if you are attempting to process your subscription and having issues.</strong></p>
 	<p>If you are renewing an existing subscription please enter the subscriber code provided on our address label above your name. Make sure you click &quot;Go&quot;.</p>
 	<p>If you are renewing, and can't find your subscriber ID, please email us on <a href="mailto:info@nzoutdoor.co.nz">info@nzoutdoor.co.nz</a>. </p>
 <?php
